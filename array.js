cef394797737a8fb3d1fdf04a112de186e4ae202
ddw1234 <dineshwagh888@gmail.com>
2023-01-30 00:47:19 +0530

    Part 9

---
+++ array.js
@@ -0,0 +1,4 @@
+var arr = ["php", "java"];
+arr[2] = "mi";
+var random = ["abc", 34, 4];
+console.warn(arr);
---
+++ array.ts
@@ -0,0 +1,5 @@
+let arr:string[]=["php","java"];
+arr[2] = "mi";
+
+let random : any[]=["abc", 34 ,4];
+console.warn(arr);
