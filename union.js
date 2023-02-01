51b549864514eb6f201ca25d3153d40add4aa38a
ddw1234 <dineshwagh888@gmail.com>
2023-02-01 23:58:56 +0530

    Part 13

---
+++ union.js
@@ -0,0 +1,10 @@
+function combine(a, b) {
+    if (typeof a === 'number' && typeof b === 'number') {
+        return a + b;
+    }
+    else {
+        return a.toString() + b.toString();
+    }
+}
+console.warn(combine(10, 20));
+console.warn(combine("a", "b"));
---
+++ union.ts
@@ -0,0 +1,13 @@
+function combine(a:number|string , b: number|string)
+{
+    if(typeof a==='number' && typeof b==='number')
+    {
+        return a + b;
+    }
+    else
+    {
+        return a.toString() + b.toString()
+    }
+}
+console.warn(combine(10,20))
+console.warn(combine("a","b"))
